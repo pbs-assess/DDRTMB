2e14a32f87dab9861620f7a9b300e4484993fe3c
robynforrest <robynforrest@gmail.com>
2024-05-27 16:03:34 -0700

    add iscam.par file, add test values and add to calcNumbersBiomass_deldiff

--- devs/run-model-v1.R
+++ devs/run-model-v1.R
@@ -111,7 +111,7 @@ par$rho <- pcod2020ctl$params[6,1] # Errors in Variables: fraction of the total 
 par$kappa <- pcod2020ctl$params[7,1] # Errors in Variables: total precision (inverse of variance) of the total error.
 # TODO: Check these are dimensioned correctly
 par$log_ft <- matrix(0, nrow=nyrs, ncol=nfleet)
-par$init_log_rec_devs <- rep(0,(dat$nage-dat$sage)) #vector(length=dat$nage - dat$sage) # I think this is length nage-sage
+par$init_log_rec_devs <- rep(0,(dat$nage-dat$sage + 1)) #vector(length=dat$nage - dat$sage + 1) # I think this is length nage-sage+1 (i.e., length 2:9)
 par$log_rec_devs <- rep(0,nyrs) #vector(length=nyrs)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -150,8 +150,24 @@ model <- function(par){
 
   # A decision was made in 2018 to fix these two parameters to be the same
   #   as ro (P. Starr). May revisit this later
-  log_avrec <- par$log_ro
-  log_recinit <- par$log_ro
+  log_avgrec <- par$log_ro
+  #log_recinit <- par$log_ro # RF: I don't think it makes sense to use this one
+
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # ~ TESTING VALUES FROM data-raw/iscam.rep and data-raw/iscam.par (2020 PCOD RESULTS) ~
+  # DELETE THIS ONCE MODEL EQUATIONS ARE TESTED
+  ro        <- 3376.55
+  steepness <- 0.809901
+  m         <- 0.306306
+  rho       <- 0.058824
+  varphi    <- 0.824621
+  log_avgrec <- log(ro)
+  Ft <- c(0.100901, 0.162811, 0.218133, 0.172062, 0.135691, 0.0919969, 0.139881, 0.0772674, 0.129473, 0.191035, 0.236091, 0.209665, 0.18531, 0.149684, 0.0791808, 0.134725, 0.135795, 0.114886, 0.152902, 0.178127, 0.167726, 0.138626, 0.128951, 0.25108, 0.210596, 0.145312, 0.137206, 0.120306, 0.104469, 0.0661442, 0.16161, 0.360395, 0.24175, 0.141831, 0.152064, 0.339467, 0.306043, 0.273561, 0.121854, 0.100894, 0.111752, 0.12076, 0.1013, 0.0790893, 0.0660031, 0.0333779, 0.037986, 0.0554713, 0.0657476, 0.0801039, 0.0748695, 0.0352134, 0.0291054, 0.0524231, 0.095605, 0.0769179, 0.0558788, 0.0562869, 0.0645536, 0.0709053, 0.0428528, 0.0288908, 0.0189564, 0.0296574, 0.0258504)
+  init_log_rec_devs <- c(-0.297834, -0.195054, -0.126748, -0.0955628, -0.0934589, -0.108359, -0.130301, 1.04734)
+  log_rec_devs <- c(1.05722, 1.10583, -0.139089, -0.165389, -0.298059, -0.336892, -0.173463, 2.84111, 0.284625, 0.163418, -0.0760200, -0.352092, -0.626335, -0.538303, -0.320139, -0.0816409, 2.69634, 0.0765257, 0.524992, 0.510128, 0.356662, 0.953328, 0.574398, 0.840802, 0.173325, 0.402038, 0.278233, -0.103700, 0.166054, 0.213154, 1.49743, 2.13800, -0.221516, -0.0713425, 0.874159, 1.27436, -0.245994, -0.775609, -0.898877, -0.701367, -0.142345, -0.829222, -0.954500, -1.11217, -1.11537, 0.209017, 0.409310, -0.409217, -0.845547, -1.24699, -1.39305, -1.25216, -0.294358, 0.668812, 0.131646, -0.489765, -0.691204, -0.667682, -0.629868, -0.792061, -0.796493, -0.646523, 0.347852, -0.110935, -0.232896)
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   # Get the Goodyear Compensation Ratio
   # This is the ratio of juvenile survival at low stock size (alpha) to unfished juvenile survival
@@ -168,7 +184,7 @@ model <- function(par){
     CR <- (5*steepness)^1.25
   }
   if(!ctl$misc[2] %in% 1:2){
-    stop("Error. S-R relationship not specified! In the controls, ctl$misc[2] must be set to 1 (BH) or 2 (Ricker).")
+    stop("S-R relationship not specified! ctl$misc[2] must be set to 1 (BH) or 2 (Ricker).")
   }
 
   # End initParameters
@@ -182,7 +198,7 @@ model <- function(par){
   #          specificed by user.
   Mt <- rep(m,nyrs) # natural mortality
   Zt <- Ft+Mt # total mortality
-  surv <- exp(-Zt) # survival rate
+  surv <- exp(-Zt) # fished survival rate
 
   # End calcTotalMortality_deldiff
 #|---------------------------------------------------------------------|
@@ -199,6 +215,12 @@ model <- function(par){
   #  with substitutions Neq = Beq/weq and R = Neq(1 - surv)
   # From SJDM, also used by Sinclair in 2005 p cod assessment
 
+  # Initialize numbers and biomass
+  numbers <- vector(length=nyrs+1)
+  biomass <- vector(length=nyrs+1)
+  log_rt <- vector(length=nage-sage + nyrs) # log recruits for entire time series including init period
+
+  # Initial Conditions
   snat <- exp(-m) # natural survival rate
   # Eqm mean weight
   wbar <- (snat*alpha.g + wk*(1-snat))/(1-snat*rho.g) # RF: calculation checked against rep file
@@ -209,7 +231,7 @@ model <- function(par){
 
   # Parameters of Stock-Recruit relationship
   # Maximum juvenile survival rate (same for BH and Ricker)
-  alpha.sr <- (CR*ro)/bo # NOTE: Steve called this so. RF thinks this is confusing notation because the o implies unfished, whereas this is max juv survival rate when the biomass is close to zero
+  alpha.sr <- (CR*ro)/bo # NOTE: *Steve called this so*. RF thinks this is confusing notation because the o implies unfished, whereas this is max juv survival rate when the biomass is close to zero
 
   # Beta parameter (density-dependence in juvenile survival)
   if(ctl$misc[2]==1){
@@ -220,6 +242,60 @@ model <- function(par){
     # Ricker
     beta.sr <- log(CR)/bo
   }
+
+  # Three options for how first year is initialized (set in ctrl$misc[5]):
+  # 0. Unfished and not at equilibrium
+  # 1. Unfished equilibrium
+  # 2. Fished and at equilibrium with Ft in first year
+  if(ctl$misc[5]==0){
+    # Unfished and not at equilibrium - initialize with age structure as in ASM
+    # Set up a vector of n-at-age for the first year (length sage:nage) with devs
+    # Then decay each age according to M
+    nAge <- vector(length=length(sage:nage))
+    for(j in 1:length(nAge)){
+      nAge[j] <- exp(log_avgrec + init_log_rec_devs[j])*exp(-Mt[1]*(j-1))
+    }
+    nAge[length(nAge)] <- nAge[length(nAge)]/(1 - exp(-Mt[1])) # plus group
+
+    ## TEST (from iscam calcNumbersAtAge)
+    #tr <- vector(length(sage:nage))
+    #tr(sage)        = ( log_avgrec(ih)+log_rec_devs(ih)(syr));
+    #tr(sage+1,nage) = (log_recinit(ih)+init_log_rec_devs(ih));
+    #tr(sage+1,nage) = tr(sage+1,nage)+log(lx(sage+1,nage));
+
+
+
+    # Add up the numbers at age to get numbers in year 1
+    numbers[1] <- sum(nAge)
+
+    log_rt[1] <- log_avgrec+log_rec_devs[1] # Check: this should be same as log(nAge[1]) but only if
+
+
+    # RFUpdate Correction: below biomass is the sum of weight at age x numbers at age not wbar
+    biomass[1] = sum(elem_prod(tmp_N,d3_wt_avg[1]))
+    annual_mean_wt[1] = biomass(ig,syr)/numbers[1]
+
+  }
+  if(ctl$misc[5]==1){
+    # Unfished equilibrium
+
+  }
+  if(ctl$misc[5]==2){
+    # Fished at equilibrium with Ft in first year
+
+  }
+  if(!ctl$misc[5] %in% 0:2){
+    stop("Starting conditions not specified! ctl$misc[5] must be set to 0, 1 or 2 (see ?pcod2020ctl for definitions).")
+  }
+
+
+
+  # Time dynamics
+
+
+
+  # Add recruitment for projection year ... assume it is average
+  rnplus=exp(log_avgrec)
 
  # End calcNumbersBiomass_deldiff
 #|---------------------------------------------------------------------|
