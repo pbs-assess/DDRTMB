054191c1dd336ca338eb719c19210e3034d04cca
robynforrest <robynforrest@gmail.com>
2024-07-24 11:33:38 -0700

    Add more plots

--- devs/Plot.r
+++ devs/Plot.r
@@ -91,7 +91,6 @@ points(yrs, pcod2020par$log_rec_devs, pch=1, col=iscamcol)
 legend("topright", legend=c("RTMB", "iscam MPD"), pch=c(19,1), col=c(rtmbcol, iscamcol) ,bty="n")
 
 # Init log rec devs
-# NOTE: RTMB ESTIMATING AN EXTRA YEAR OF INIT_LOG_REC_DEVS
 inityrs <- (yrs[1]-length(pcod2020par$init_log_rec_devs)):(yrs[1]-1)
 maxY <- max(c(pcod2020par$init_log_rec_devs,(pl$init_log_rec_devs+2*plsd$init_log_rec_devs)))
 minY <- min(c(pcod2020par$init_log_rec_devs,(pl$init_log_rec_devs-2*plsd$init_log_rec_devs)))
@@ -101,6 +100,33 @@ arrows(x0=inityrs, y0=(pl$init_log_rec_devs-2*plsd$init_log_rec_devs), x1 = init
 points(inityrs, pcod2020par$init_log_rec_devs, pch=1, col=iscamcol)
 legend("topright", legend=c("RTMB", "iscam MPD"), pch=c(19,1), col=c(rtmbcol, iscamcol) ,bty="n")
 
+# Derived recruits
+recyrs <- (dat$syr+dat$sage):dat$nyr
+maxY <- max(c(pcod2020rep$rt,(plr$rt+2*plrsd$rt)))
+plot(recyrs, plr$rt, pch=19, col=rtmbcol, ylim=c(0,maxY), xlab="Years", ylab="Recruits")
+arrows(x0=recyrs, y0=(plr$rt-2*plrsd$rt), x1 = recyrs, y1=(plr$rt+2*plrsd$rt), code = 0, col=rtmbcol)
+points((recyrs+0.5), pcod2020rep$rt, pch=19, col=iscamcol)
+legend("topright", legend=c("RTMB", "iscam MPD"), pch=19, col=c(rtmbcol, iscamcol) ,bty="n")
+
+# Log ft pars
+maxY <- max(c(pcod2020par$log_ft_pars,(pl$log_ft_pars+2*plsd$log_ft_pars)))
+minY <- min(c(pcod2020par$log_ft_pars,(pl$log_ft_pars-2*plsd$log_ft_pars)))
+plot(yrs, pl$log_ft_pars, pch=19, cex=1.2, col=rtmbcol, ylim=c(minY,maxY), xlab="Years", ylab="Log ft pars")
+abline(h=0, lty=2, lwd=0.5)
+arrows(x0=yrs, y0=(pl$log_ft_pars-2*plsd$log_ft_pars), x1 = yrs, y1=(pl$log_ft_pars+2*plsd$log_ft_pars), code = 0, col=rtmbcol)
+points(yrs, pcod2020par$log_ft_pars, pch=1, col=iscamcol)
+legend("topright", legend=c("RTMB", "iscam MPD"), pch=c(19,1), col=c(rtmbcol, iscamcol) ,bty="n")
+
+# Derived Ft pars
+maxY <- max(c(exp(pcod2020par$log_ft_pars),(exp(pl$log_ft_pars))))
+plot(yrs, exp(pl$log_ft_pars), type="l", lwd=3,col=rtmbcol, ylim=c(0,maxY), xlab="Years", ylab="Derived ft")
+lines(yrs, exp(pcod2020par$log_ft_pars), lwd=3, col=iscamcol)
+legend("topright", legend=c("RTMB", "iscam MPD"), lwd=3, col=c(rtmbcol, iscamcol) ,bty="n")
 
 # Parameters
-
+Pars <- matrix(nrow=2, ncol=5)
+Pars[1,] <- c(pl$log_ro, pl$h, pl$log_m, pl$rho, pl$kappa)
+Pars[2,] <- c(pcod2020par$theta1,pcod2020par$theta2,pcod2020par$theta3,pcod2020par$theta6,pcod2020par$theta7)
+colnames(Pars) <- c("log_ro", "h", "log_m", "rho", "kappa")
+barplot(Pars, beside=T, col=c(rtmbcol, iscamcol), ylim=c(min(Pars),max(Pars)))
+legend("topright", legend=c("RTMB", "iscam MPD"), pch=15, col=c(rtmbcol, iscamcol) ,bty="n")
