cab674f209eb22b52605a0b1c43f7a2edb82b309
robynforrest <robynforrest@gmail.com>
2024-07-29 10:31:47 -0700

    Make ft compare plot

--- devs/plot_iscam_compare_mcmc.r
+++ devs/plot_iscam_compare_mcmc.r
@@ -38,16 +38,12 @@ mcmcdiagnostics <- readRDS(here("outputs","MCMCDiagnostics.rda"))
 dat <- pcod2020dat
 ctl <- pcod2020ctl
 
-# 2. iscam Read in MCMC outputs - need to remove burnin
+# 2. iscam Read in MCMC outputs - **need to remove burnin**
 iscampars <- read_csv(here("data-raw/iscam_mcmc.csv"))
 iscambiomass <- read_csv(here("data-raw/iscam_sbt_mcmc.csv"))
 iscamrecruits <- read_csv(here("data-raw/iscam_rt_mcmc.csv"))
 iscamrecdevs <- read_csv(here("data-raw/iscam_rdev_mcmc.csv"))
 iscamfmort <- read_csv(here("data-raw/iscam_ft_mcmc.csv"))
-
-# Pairs and trace plots
-# Trace plots
-
 
 # Parameter histograms (without and with priors)
 # 1. Simple individual histograms
@@ -58,6 +54,7 @@ par(mfrow=c(2,3))
 for(i in 1:dat$nit) hist(mcmcderived$q[,i])
 
 # Biomass, recruits and fishing mortality time series
+# Biomass
 post_biomass_rtmb <- mcmcderived$biomass %>%
   apply(2,quantile,probs=c(0.025,0.5,0.975))%>%
   t() %>%
@@ -69,7 +66,6 @@ post_biomass_rtmb <- mcmcderived$biomass %>%
   geom_line(aes(x=Year,y=med),color="blue")+
   xlab("Year") + ylab("Posterior biomass (t)")+ggtitle("rtmb")+
   theme_pbs()
-#print(post_biomass_rtmb)
 
 post_biomass_iscam <- iscambiomass[1001:2000,] %>%
   apply(2,quantile,probs=c(0.025,0.5,0.975)) %>%
@@ -84,3 +80,32 @@ post_biomass_iscam <- iscambiomass[1001:2000,] %>%
   theme_pbs()
 #print(post_biomass_iscam)
 cowplot::plot_grid(post_biomass_rtmb,post_biomass_iscam, ncol=1)
+
+# Fishing mortality
+post_ft_rtmb <- exp(mcmcpars[,4:68]) %>%
+  apply(2,quantile,probs=c(0.025,0.5,0.975))%>%
+  t() %>%
+  as.data.frame() %>%
+  rename(lwr=`2.5%`, med=`50%`, upr=`97.5%`) %>%
+  mutate(Year=dat$syr:dat$nyr) %>%
+  ggplot() +
+  geom_ribbon(aes(x=Year, ymin=lwr, ymax=upr), fill="blue", alpha = 0.3) +
+  geom_line(aes(x=Year,y=med),color="blue")+
+  xlab("Year") + ylab("Posterior fishing mortality")+ggtitle("rtmb")+
+  theme_pbs()
+#print(post_ft_rtmb)
+
+# Only take gear 1 in (ft for surveys is all 0)
+post_ft_iscam <- iscamfmort[1001:2000,1:(dat$nyr-dat$syr+1)] %>%
+  apply(2,quantile,probs=c(0.025,0.5,0.975)) %>%
+  t() %>%
+  as.data.frame() %>%
+  rename(lwr=`2.5%`, med=`50%`, upr=`97.5%`) %>%
+  mutate(Year=dat$syr:dat$nyr) %>%
+  ggplot() +
+  geom_ribbon(aes(x=Year, ymin=lwr, ymax=upr), fill="red", alpha = 0.3) +
+  geom_line(aes(x=Year,y=med),color="red")+
+  xlab("Year") + ylab("Posterior biomass (t)")+ggtitle("iscam")+
+  theme_pbs()
+#print(post_ft_iscam)
+cowplot::plot_grid(post_ft_rtmb,post_ft_iscam, ncol=1)
